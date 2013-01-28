9a512d529cb69ba3d7908a442f440aca5c255623
Thomas Kahle <tomka@gentoo.org>
2013-01-28 13:30:39 +0100

    first steps ValueTree testing

--- src/zsolve/unittests/ValueTree.cpp
+++ src/zsolve/unittests/ValueTree.cpp
@@ -1,14 +1,21 @@
 #include <gtest/gtest.h>
 
-bool vt () { return true; };
+#include "../ValueTree.hpp"
 
-TEST(ValueTree, create) {
-    EXPECT_EQ(true, vt());
-    EXPECT_FALSE(!(vt()));
+class ValueTreeTest : public ::testing::Test {
+protected:
+    virtual void SetUp() {
+	// Initialize ValueTrees if needed
+    }
+
+    // virtual void TearDown() {}
+
+    _4ti2_zsolve_::ValueTree<int> t0_;
+    _4ti2_zsolve_::ValueTree<int> t1_;
+    _4ti2_zsolve_::ValueTree<int> t2_;
+};
+
+TEST_F(ValueTreeTest, EmptyTree) {
+    EXPECT_EQ(NULL, t0_.zero);
+    EXPECT_EQ(-1, t0_.level);
 }
-
-TEST(ValueTree, destroy) {
-    EXPECT_EQ(true, vt());
-    EXPECT_FALSE(!(vt()));
-}
-
