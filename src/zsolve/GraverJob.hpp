59c74767b8c5d63787328c9b398efa05e0a362c8
Thomas Kahle <thomas-kahle@gmx.de>
2013-02-01 14:59:38 +0100

    Framework for parallel Graver computation implemented

---
+++ src/zsolve/GraverJob.hpp
@@ -0,0 +1,98 @@
+/*
+4ti2 -- A software package for algebraic, geometric and combinatorial
+problems on linear spaces.
+
+Copyright (C) 2013 4ti2 team.
+Main author(s): Thomas Kahle.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA. 
+*/
+
+#ifndef _4ti2_zsolve__GraverJob_
+#define _4ti2_zsolve__GraverJob_
+
+#include <iostream>
+#include <cassert>
+
+#include "Lattice.hpp"
+
+namespace _4ti2_zsolve_
+{
+
+/**
+ * \brief Graver job computation
+ * 
+ * Input:
+ *   * lattice basis L for \Lattice\subseteq\Z^n (typically, a coordinate projection of ker(A))
+ *   * sets of vectors G_r,G_s\in\Lattice with 1-norms r,s on first d components
+ *   * the auxiliary information for each vector should be given
+ *   from master to slave!
+ *   * d
+ *   * n = number of variables/components (Typically, we have d=n-1.)
+ * 
+ * Output:
+ *   * all sums v+w \in G_r+G_s that are minimal elements in \Lattice\setminus\{0\}
+ * 
+ * Algorithm:
+ *   * irr={}
+ *   * for all pairs (v,w) \in G_r+G_s {
+ *     * if v+w is minimal in \Lattice\setminus\{0\} {
+ *       * add auxiliary data to v+w like support, norm, ...
+ *       * set irr=irr\cup{v+w}
+ *     }
+ *   }
+ *   * return irr (including auxiliary data for each vector)
+ * 
+ * 
+ * Remarks:
+ * 
+ * * Clearly, if r=s, only half the number of pairs have to be
+ * checked.
+ * * If v and w have the same sign pattern in the last n-d
+ * components, the quick test applies and v+w is reducibly by both
+ * v and w and can thus be discarded immediately.
+ * * Consequently, if d=n-1, it is worth pre-sorting G_r and G_s
+ * according to the last component being >0, =0, <0.
+ * * For the minimality test, we could use the lattice basis and
+ * look for a nonzero vector u<>v+w in \Lattice with the same
+ * sign-pattern as v+w and with u<=v+w in this orthant.
+ * * We may also give the slave G_1, G_2, ...up to some norm for a
+ * quick check.
+ * 
+ * 
+ */
+
+// template <typename T>
+// VectorArray<T>*
+// graverJob () {
+//     return new VectorArray<T>;
+// }
+
+template <typename T> 
+VectorArray<T> *
+graverJob (const VectorArray<T>& Gr,
+	   const VectorArray<T>& Gs,
+	   const Lattice<T>& current_gens,
+	   const size_t& maxvar) 
+{
+    size_t num_vars = current_gens.num_variables();
+    VectorArray<T> *result = new VectorArray<T> (num_vars);
+    result->append_vector ( create_zero_vector<T> (num_vars));
+    return result;
+}
+
+} // namespace _4ti2_zsolve_
+
+#endif
--- src/zsolve/ParallelGraver.hpp
+++ src/zsolve/ParallelGraver.hpp
@@ -23,23 +23,39 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 #ifndef _4ti2_zsolve__ParallelGraver_
 #define _4ti2_zsolve__ParallelGraver_
 
+#include <algorithm>
+#include <future>
+#include <map>
+#include <stdexcept>
+
 #include "zsolve/Algorithm.hpp"
 #include "zsolve/BitSet.h"
-#include "zsolve/LinearSystem.hpp"
 #include "zsolve/Controller.hpp"
+#include "zsolve/GraverJob.hpp"
+#include "zsolve/LinearSystem.hpp"
+#include "zsolve/Norms.hpp"
+#include "zsolve/Vector.hpp"
 
 namespace _4ti2_zsolve_
 {
+
+// Binary search tree for current lattice vectors, sorted according to
+// (sub)-norms.  This is the way to do it in the future, but only
+// supported in gcc-4.7 and up.  
+// template <typename T> using NormBST = std::map<T, VectorArray<T>* >;
 
 template <typename T> class Controller;
 
 template <typename T> 
 class ParallelGraver : public Algorithm <T>
 {
-private:
-    size_t m_num_variables;
+    typedef std::map <T, VectorArray<T>* > NormBST;
 
-    void localInit ();
+private:
+    size_t m_num_variables; ///< Number of variables in the Graver problem
+
+    void localInit (); ///< Common initialization of local data
+    NormBST* createNormBST (size_t dims);
 
 protected:
     Controller <T> * m_controller; ///< Controller for computation (unused until now)
@@ -117,16 +133,146 @@ ParallelGraver<T>::localInit (){
     m_num_variables = m_current_gens->num_variables();
 }
 
+/** 
+ * \brief Update the binary search tree
+ * 
+ * @param dims How many dimensions to consider
+ * 
+ * @return new binary search tree for norms.
+ */
+template <typename T>
+std::map< T, VectorArray<T>* >* 
+ParallelGraver<T>::createNormBST (size_t dims) {
+    /// @TODO: Parallelize this computation by splitting the todolist
+    typedef std::map <T, VectorArray<T>* > NormBST;
+    NormBST *result = new NormBST ();
+
+    for (size_t i = 0; i < m_current_gens->num_vectors(); i++){
+	T current_norm = norm_vector <T> ( (*m_current_gens)[i], dims);
+	VectorArray<T> *current_vectors = NULL;
+	// The map container gives a strong guarentee of not being
+	// changed
+	try {
+	    current_vectors = result->at(current_norm);
+	    current_vectors->append_vector( copy_vector<T> ( (*m_current_gens)[i], 
+							     m_num_variables) );
+	}
+	catch (std::out_of_range& oor) {
+	    current_vectors = new VectorArray<T> ();
+	    current_vectors->append_vector( copy_vector<T> ( (*m_current_gens)[i], 
+							     m_num_variables) );
+	    result->insert( std::pair <T, VectorArray<T>* > 
+			    (current_norm, current_vectors) );
+	};
+    }
+    return result;
+}
+
 template <typename T>
 void 
 ParallelGraver<T>::compute (int backup_frequency)
 {
-    // The big loop over the variables
-    for (size_t varindex = 0; varindex < m_num_variables; varindex++){
-	std::cout << "Now doing variable " << varindex << std::endl;
-	/// ... 
-    }
+    typedef std::map <T, VectorArray<T>* > NormBST;
 
+    // First step: Add all negatives to basis.  This makes for
+    // sign-consistent representations on the first variable:
+    m_current_gens->append_negatives();
+
+    // Now comes the big loop over the remaining variables
+    for (size_t varindex = 1; varindex < m_num_variables; varindex++){
+	std::cout << "Now doing variable " << varindex+1 << std::endl;
+
+	NormBST *s = createNormBST(1);
+	std::cout << "Number of different norms: " << s->size() << std::endl;
+
+	T max_norm = s->rbegin()->first;
+	std::cout << "Maximum norm: " << max_norm << std::endl;
+
+	std::vector< NormPair<T> > jobs;
+	// If there are degree one vectors, then we have our first job: combine them
+	if (s->cbegin()->first == 1) 
+	    jobs.push_back( NormPair<T>(1,1) );
+	T completed_norm = 1;
+	while (completed_norm < 2*max_norm) {
+	    // Check if there are any not done jobs at the current level
+	    std::vector < std::future < VectorArray<T>* > > m_futures;
+	    for (auto it = jobs.begin(); it != jobs.end(); it++) {
+		if ((*it).sum <= completed_norm+1) {
+			// do it!
+			std::cout << "DenkDenkDenk... " << (*it).sum << std::endl;
+			// A job with the lowest sum is always in the beginning of the
+			// jobs vector: (Check?)
+
+			// Check if there are vectors with this norm at all:
+			if (s->find(it->first) != s->end() && s->find(it->second) != s->end()) {
+			    // Now we call the future.  This is tricky.  Since graverJob is a
+			    // function template it can not be passed to async.  Instead we
+			    // create an anonymous function that captures all relevant data
+			    // and calls graverJob.  The compiler will then instantiate
+			    // graverJob.
+			    std::future < VectorArray<T>* > fut = std::async(
+				std::launch::async, // Compiler can decide launch order?
+				[s,m_current_gens, varindex, it] () {
+				    std::cout << "Creating job"<< std::endl;
+				    return graverJob (*(s->at(it->first)), 
+						      *(s->at(it->second)), 
+						      *(m_current_gens), 
+						      varindex);});
+			    // Wait for everybody to do their jobs NOW?
+                            // fut.wait();
+			    m_futures.push_back (std::move(fut)); // Lose ownership of the future
+			}
+		    }
+	    }
+	    // Synchronize
+	    std::cout << "Waiting for results ...";
+	    for (auto it = m_futures.begin(); it != m_futures.end(); ++it)
+		it->wait();
+	    std::cout << "Done." << std::endl;
+	    // Retrieve results
+	    for (auto it = m_futures.begin(); it != m_futures.end(); ++it){
+		VectorArray<T> *result = (*it).get();
+		if (result->num_vectors() == 0) // nothing new
+		    continue;
+		T norm = norm_vector<T> ((*result)[0], varindex); // Workaround, norm should be part of result
+		std::cout << "Current Norm : " << norm << std::endl;
+		// Check if maximum norm has increased
+		if (max_norm < norm) {
+		    std::cout << "New norm bound : " << norm << std::endl;
+		    max_norm = norm;
+		}
+		// Join the result with what is already saved in s under this norm
+		if (s->find(norm) == s->end()){
+		    // not found
+		    s->insert( std::pair <T, VectorArray<T>* > 
+			       (norm, result));
+		    // Ownership transferred to s
+		} 
+		else { 
+		    // found this norm, append vectors.
+		    /// @TODO Change this copy to a move!
+		    for (size_t vc = 0; vc < result->num_vectors(); vc++)
+			s->at(norm)->append_vector ( copy_vector<T> ((*result)[vc], m_num_variables));
+		    delete result; // Must then not delete the vectors
+		}
+	    }
+	    // Done with futures, clean up:
+	    m_futures.clear();
+
+	    // Now all jobs with norm sum <= completed_norm+1 are done.
+	    // Schedule new jobs:
+	    completed_norm++;
+	    // Add new jobs for each norm pair (i, completed_norm), i=
+	    // 1..completed_norm such that there are moves in the
+	    // respective degrees.
+	    if (s->find(completed_norm) != s->end())
+		for (T i = 1; i <= completed_norm; i++)
+		    if (s->find(i) != s->end())
+			jobs.push_back ( NormPair<T> (i, completed_norm));
+	    // Keep jobs sorted according to total norm
+	    std::sort(jobs.begin(), jobs.end());
+	}
+    }
 }
 
 template <typename T>
@@ -144,10 +290,16 @@ ParallelGraver<T>::extract_graver_results (VectorArray<T> &graver) {
 	T* vector = (*m_current_gens)[i];
 	T* result = copy_vector <T> (vector, m_num_variables);
 
-	std::cout << i << std::endl;
-	print_vector (std::cout, result, m_num_variables);
-	std::cout << std::endl;
-	graver.append_vector (result);
+	// In the output we remove symmetric copies
+	bool has_symmetric = true;
+	// Check if there is a symmetric vector
+	for (size_t j = 0; j < m_num_variables; j++)
+	    if (!m_current_gens->get_variable (j).check_bounds (-vector[j]))
+		has_symmetric = false;
+	// Check if this vector or its negative should be removed:
+	int lex_cmp = lex_compare_vector_with_negative (vector, m_num_variables);
+	if (!has_symmetric || lex_cmp > 0)
+	    graver.append_vector (result);
     }
     
     if (m_controller != NULL)
